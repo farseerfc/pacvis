8127b7a21373c26d09b58abe5c52384bacb64f1b
Jiachen Yang <farseerfc@gmail.com>
2016-07-20 02:51:05 +0900

    add custom js css

---
+++ pacvis/static/pacvis.js
@@ -0,0 +1,106 @@
+function filesize(size) {
+  var units = "KMGT";
+  var left = size;
+  var unit = -1;
+  for (; left > 1100 && unit < 3; unit++) {
+    left /= 1024;
+  }
+  if (unit === -1) {
+    return size + "B";
+  } else {
+    if (size < 0)
+      left = -left;
+    return Math.round(left * 100) / 100 + units[unit] + "iB";
+  }
+}
+
+function size2value(size) { return Math.sqrt(Math.sqrt(size)) / 5; }
+
+function createPkgListDom(list) {
+  let depsdom = "";
+  if (list == "")
+    return "";
+  for (let dep of list.split(", ")) {
+    depsdom += "<button onclick='document.getElementById(\"search\").value=\"" +
+               dep + "\";trysearch()'>" + dep + "</button>";
+  }
+  return depsdom;
+}
+
+function selectPkg(node) {
+  document.getElementById("pkgname").innerHTML = node.label;
+
+  document.getElementById("fsinfo").style.visibility = "visible";
+
+  var selectsize = document.getElementById("selectsize");
+  document.getElementById("pkgsize").innerHTML =
+      selectsize.options[selectsize.selectedIndex].text + ": " +
+      filesize(node[selectsize.value]);
+  let reason = node.group == "normal" ? "as a dependency" : "explicitly";
+  document.getElementById("pkgreason").innerHTML = reason;
+  document.getElementById("pkgversion").innerHTML = node.version;
+  document.getElementById("pkgdesc").innerHTML = node.desc;
+  document.getElementById("pkglevel").innerHTML = node.level;
+  // document.getElementById("togglehide").style.visibility = "visible";
+  // document.getElementById("togglehide").innerHTML =
+  //     node.hidden ? "Show" : "Hide";
+
+  // document.getElementById("fsdeps").style.visibility = "visible";
+  document.getElementById("pkgdeps").innerHTML = createPkgListDom(node.deps);
+  // document.getElementById("fsreqs").style.visibility = "visible";
+  document.getElementById("pkgreqs").innerHTML = createPkgListDom(node.reqs);
+  // document.getElementById("fsoptdeps").style.visibility = "visible";
+  document.getElementById("pkgoptdeps").innerHTML =
+      createPkgListDom(node.optdeps);
+}
+
+function togglehide() {
+  let pkgname = document.getElementById("pkgname").innerHTML;
+  for (let node of nodes.get()) {
+    if (node.label == pkgname) {
+      var hide = !node.hidden;
+      nodes.update({id : node.id, hidden : hide});
+      for (let edge of edges.get()) {
+        if (edge.from == node.id) {
+          edges.update(
+              {id : edge.id, hidden : hide || nodes.get()[edge.to].hidden});
+        }
+        if (edge.to == node.id) {
+          edges.update(
+              {id : edge.id, hidden : nodes.get()[edge.from].hidden || hide});
+        }
+      }
+      selectPkg(node);
+      network.stabilize(50);
+    }
+  }
+}
+
+function trysearch() {
+  let pkgname = document.getElementById("search").value;
+  for (let node of nodes.get()) {
+    if (node.label == pkgname) {
+      network.selectNodes([ node.id ]);
+      selectPkg(node);
+      if (!node.hidden) {
+        network.focus(node.id, {
+          scale : Math.log(nodes.length) / 5,
+          locked : false,
+          animation : {duration : 500, easingFunction : "linear"}
+        });
+      }
+    }
+  }
+}
+
+function switchsize() {
+  let selectsize = document.getElementById("selectsize");
+  let size = selectsize.options[selectsize.selectedIndex].value;
+  let pkgname = document.getElementById("pkgname").innerHTML;
+  for (let node of nodes.get()) {
+    nodes.update({id : node.id, value : size2value(node[size])});
+    if (node.label == pkgname) {
+      selectPkg(node);
+    }
+  }
+}
