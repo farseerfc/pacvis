20a49e19b47b65df35ef6e26c10bcbcb87c59383
Jiachen Yang <farseerfc@gmail.com>
2016-08-12 19:38:47 +0900

    implemented vdeps

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup, find_packages
 
 setup(name='PacVis',
       version='0.2.0',
-      description='Visualize pacman local database using Vis.js, inspired by pacgraph',
+      description='Visualize pacman local database using Vis.js,'+
+        ' inspired by pacgraph',
       author='Jiachen Yang',
       author_email='farseerfc@archlinuxcn.org',
       url='https://pacvis.farseerfc.me/',
