cdd6b5d5c769b57707237b444a5cbe0542422f5b
Jiachen Yang <farseerfc@gmail.com>
2017-08-18 15:32:16 +0900

    output using log format if stderr isatty

--- pacvis/console.py
+++ pacvis/console.py
@@ -18,33 +18,42 @@ def start_message(s):
     global last_message, head_message, last_line_ended
     head_message = s
     last_message = s
-    if not last_line_ended:
-        sys.stderr.write("\n")
-    sys.stderr.write(s)
-    sys.stderr.flush()
+    if sys.stderr.isatty():
+        if not last_line_ended:
+            sys.stderr.write("\n")
+        sys.stderr.write(s)
+        sys.stderr.flush()
     last_line_ended = False
 
 
 def append_message(s):
     global last_message, head_message, log_file, last_line_ended
 
-    sys.stderr.write("\r" + (" " * len(last_message)))
+    if sys.stderr.isatty():
+        sys.stderr.write("\r" + (" " * len(last_message)))
     last_message = head_message + s
-    sys.stderr.write("\r" + last_message)
-    sys.stderr.flush()
+    if sys.stderr.isatty():
+        sys.stderr.write("\r" + last_message)
+        sys.stderr.flush()
     with log_file() as log:
         log.write("%s: %s\n" % (strftime(time_format, gmtime()), last_message))
         log.flush()
+        if not sys.stderr.isatty():
+            sys.stderr.write("%s: %s\n" % (strftime(time_format, gmtime()), last_message))
     last_line_ended = False
 
 
 def print_message(s):
     global last_line_ended
     if not last_line_ended:
-        sys.stderr.write("\n")
-    sys.stderr.write(s + "\n")
-    sys.stderr.flush()
+        if sys.stderr.isatty():
+            sys.stderr.write("\n")
+    if sys.stderr.isatty():
+        sys.stderr.write(s + "\n")
+        sys.stderr.flush()
     with log_file() as log:
         log.write("%s: %s\n" % (strftime(time_format, gmtime()), s))
         log.flush()
+        if not sys.stderr.isatty():
+            sys.stderr.write("%s: %s\n" % (strftime(time_format, gmtime()), s))
     last_line_ended = True
